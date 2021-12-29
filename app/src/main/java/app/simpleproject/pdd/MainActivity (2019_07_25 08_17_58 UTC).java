bfc6fd390744e881dfbf506ee33d8c3e204afc1a
lesnikvo <nikitalesnikov@outlook.com>
2021-12-29 20:28:42 +0500

    init

---
+++ app/src/main/java/app/simpleproject/pdd/MainActivity (2019_07_25 08_17_58 UTC).java
@@ -0,0 +1,39 @@
+package app.simpleproject.pdd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // получим идентификатор выбранного пункта меню
+        int id = item.getItemId();
+
+        // Операции для выбранного пункта меню
+        switch (id) {
+            case R.id.signOut:
+              //  mAuth.signOut();
+               // updateUI(null);
+                Toast.makeText(MainActivity.this, "Вышли", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
